14e9b6c81c6f88c4d70d1a160ea2fcdbb08a9b8e
Helson <hcssilva@gmail.com>
2024-11-18 12:22:32 -0300

    Alterei o primeiro echo

--- index.php
+++ index.php
@@ -1,10 +1,6 @@
 <?php
 
-<<<<<<< HEAD
 echo "<strong>Aló Mundo PHP!! 456</strong>";
 echo "<strong>Mudança arquivo git!!</strong>";
-=======
-echo "<strong>Aló Mundo PHP!! 123</strong>";
-echo "<strong>Mudança arquivo PHP!!</strong>";
->>>>>>> b55af09bc85882b5197d7bbfb2ffe83cd263db1d
+
 ?>
